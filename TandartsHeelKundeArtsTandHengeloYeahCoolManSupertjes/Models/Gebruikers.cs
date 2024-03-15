6582e4370e1dd8cb1acf56a9a77aef57e2737811
joernepoerne <0327462@student.rocvantwente.nl>
2024-03-15 11:10:03 +0100

    barncha initial

---
+++ TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes/Models/Gebruikers.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes.Models
+{
+    public class Gebruikers
+    {
+        public int gebruikersID { get; set; }
+        [StringLength(50, ErrorMessage = "Maximumlengte van {0} is {1} tekens")]
+        public string VoorNaam { get; set; }
+        public string Achternaam { get; set; }
+
+    }
+}
