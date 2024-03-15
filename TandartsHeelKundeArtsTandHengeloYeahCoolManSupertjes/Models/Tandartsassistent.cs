53b987bacf947b87ebffeca9503b286f274ba951
huubd <huub.dorgelo@gmail.com>
2024-03-15 11:55:27 +0100

    tandarts en tandartsassistent models

---
+++ TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes/Models/Tandartsassistent.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes.Models
+{
+    public class Tandartsassistent
+    {
+        public int ID { get; set; }
+        public Gebruikers Gebruiker { get; set; }
+    }
+}
