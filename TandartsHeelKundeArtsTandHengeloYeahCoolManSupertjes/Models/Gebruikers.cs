0a26d58f16a9508a3325c933b0935b8bd1177a23
joernepoerne <0327462@student.rocvantwente.nl>
2024-03-15 11:47:58 +0100

    Gebruikers model

--- TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes/Models/Gebruikers.cs
+++ TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes/Models/Gebruikers.cs
@@ -5,10 +5,19 @@ namespace TandartsHeelKundeArtsTandHengeloYeahCoolManSupertjes.Models
 {
     public class Gebruikers
     {
-        public int gebruikersID { get; set; }
         [StringLength(50, ErrorMessage = "Maximumlengte van {0} is {1} tekens")]
-        public string VoorNaam { get; set; }
+        [Key]
+        public required int gebruikersID { get; set; }
+        public string Voornaam { get; set; }
         public string Achternaam { get; set; }
-
+        public string Tussenvoegsel { get; set; }
+        public string Adres { get; set; }
+        public string Postcode { get; set; }
+        public string Woonplaats { get; set; }
+        public DateOnly Geboortedatum { get; set; }
+        public string zorgverzekeraar { get; set; }
+        public int Klantnummer { get; set; }
+        public string Email { get; set; }
+        public int Telefoon {  get; set; }
     }
 }
